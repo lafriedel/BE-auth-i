dece75c71555a9c4d9ffb5b39d3a7d52dfb2a6aa
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-25 16:24:53 -0500

    Successful GET to /api/users with authorization middleware

--- index.js
+++ index.js
@@ -59,12 +59,12 @@ server.post("/api/login", (req, res) => {
       });
   } else {
     res
-      .status(404)
+      .status(400)
       .json({ error: "You must provide both a username and password." });
   }
 });
 
-server.get("/api/users", (req, res) => {
+server.get("/api/users", authorize, (req, res) => {
   db("users")
     .then(users => {
       res.status(200).json(users);
@@ -73,5 +73,29 @@ server.get("/api/users", (req, res) => {
       res.status(500).json({ error: "There was an error." });
     });
 });
+
+function authorize(req, res, next) {
+    const { username, password } = req.headers;
+
+    if (username && password) {
+        db("users")
+          .where("username", username)
+          .first()
+          .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+              next();
+            } else {
+              res.status(401).json({ message: "Invalid credentials." });
+            }
+          })
+          .catch(err => {
+            res.status(500).json({ error: "There was an error." });
+          });
+      } else {
+        res
+          .status(400)
+          .json({ error: "You must provide both a username and password." });
+      }
+}
 
 server.listen(8000, () => console.log("Server listening on port 8000"));
