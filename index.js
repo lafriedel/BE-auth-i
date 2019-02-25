df41f0167fec59f93f781a4317e5c3d28426aa20
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-02-25 16:18:37 -0500

    Successful POST at /api/login with working error messages

--- index.js
+++ index.js
@@ -40,6 +40,30 @@ server.post("/api/register", (req, res) => {
   }
 });
 
+server.post("/api/login", (req, res) => {
+  let { username, password } = req.body;
+
+  if (username && password) {
+    db("users")
+      .where("username", username)
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: "Invalid credentials." });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: "There was an error." });
+      });
+  } else {
+    res
+      .status(404)
+      .json({ error: "You must provide both a username and password." });
+  }
+});
+
 server.get("/api/users", (req, res) => {
   db("users")
     .then(users => {
